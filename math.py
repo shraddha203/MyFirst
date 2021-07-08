f9a9ee147ab5a35cbcfdd1dc95f1113a373c6f40
shraddha203 <64681004+shraddha203@users.noreply.github.com>
2021-07-08 14:28:39 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,15 @@
+#add funtion implementation
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y    #on master branch
+def multiply(x,y):
+    return x*y     #on Bug456 branch
+def divide(x,y):
+    if y==0:       #on Bug 789
+        return DIVIDE_BY_ZERO_ERROR
+    else 
+        return x/y
+#square implementation 
+def square(x)
+    pass
